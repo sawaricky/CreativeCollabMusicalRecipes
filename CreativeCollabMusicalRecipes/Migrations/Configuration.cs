bfc89232752d2a437f182d76e65f45fcf480d6f1
akash sharma <akashsharma3799@gmail.com>
2024-07-19 14:01:38 -0400

    Add project files.

---
+++ CreativeCollabMusicalRecipes/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CreativeCollabMusicalRecipes.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CreativeCollabMusicalRecipes.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CreativeCollabMusicalRecipes.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ CreativeCollabMusicalRecipes/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CreativeCollabMusicalRecipes.Startup))]
+namespace CreativeCollabMusicalRecipes
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
